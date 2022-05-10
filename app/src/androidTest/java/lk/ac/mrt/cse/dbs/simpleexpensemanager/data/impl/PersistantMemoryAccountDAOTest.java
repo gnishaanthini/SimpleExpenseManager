d31ea9c97a5a20aedcd75ac1c3c7e38f6f1f3899
gnishaanthini <nishaanthini.19@cse.mrt.ac.lk>
2022-05-10 18:51:43 +0530

    commit new changes

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistantMemoryAccountDAOTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistantMemoryAccountDAOTest.java
@@ -20,12 +20,12 @@ public class PersistantMemoryAccountDAOTest {
         String bankName = "BMN";
         String accountHolderName = "XYZ";
         Double balance = 12000.0;
-        Account newAccount= new Account(accountNo,bankName,accountHolderName,balance);
-        PersistantMemoryAccountDAO testAccDAO= new PersistantMemoryAccountDAO(ApplicationProvider.getApplicationContext());
-        testAccDAO.addAccount(newAccount);
-        Account output= testAccDAO.getAccount(accountNo);
+        Account account1= new Account(accountNo,bankName,accountHolderName,balance);
+        PersistantMemoryAccountDAO test1= new PersistantMemoryAccountDAO(ApplicationProvider.getApplicationContext());
+        test1.addAccount(account1);
+        Account output= test1.getAccount(accountNo);
         try {
-            assertEquals(newAccount,output);
+            assertEquals(account1,output);
         }
         catch (AssertionError e) {
 
@@ -38,12 +38,12 @@ public class PersistantMemoryAccountDAOTest {
         String bankName = "BMN";
         String accountHolderName = "XYZ";
         Double balance = 12000.0;
-        Account newAccount= new Account(accountNo,bankName,accountHolderName,balance);
-        PersistantMemoryAccountDAO testAccDAO= new PersistantMemoryAccountDAO(ApplicationProvider.getApplicationContext());
-        testAccDAO.addAccount(newAccount);
-        Account output= testAccDAO.getAccount(accountNo);
+        Account account2= new Account(accountNo,bankName,accountHolderName,balance);
+        PersistantMemoryAccountDAO test2= new PersistantMemoryAccountDAO(ApplicationProvider.getApplicationContext());
+        test2.addAccount(account2);
+        Account output= test2.getAccount(accountNo);
         try {
-            assertEquals(newAccount,output);
+            assertEquals(account2,output);
         }
         catch (AssertionError e) {
 
