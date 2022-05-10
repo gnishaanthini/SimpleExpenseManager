16884466e1d87ad8362c8bc9db6b5e9955e2e54c
gnishaanthini <91202473+gnishaanthini@users.noreply.github.com>
2022-05-10 17:47:54 +0530

    Add files via upload

---
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistantMemoryAccountDAOTest.java
@@ -0,0 +1,52 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
+
+//import android.app.Activity;
+
+import static org.junit.Assert.*;
+import androidx.test.core.app.ApplicationProvider;
+
+//import org.testng.annotations.Test;
+import org.junit.Test;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+
+
+public class PersistantMemoryAccountDAOTest {
+
+    @Test
+    public void getAccount() throws InvalidAccountException {
+        String accountNo="555693B";
+        String bankName = "BMN";
+        String accountHolderName = "XYZ";
+        Double balance = 12000.0;
+        Account newAccount= new Account(accountNo,bankName,accountHolderName,balance);
+        PersistantMemoryAccountDAO testAccDAO= new PersistantMemoryAccountDAO(ApplicationProvider.getApplicationContext());
+        testAccDAO.addAccount(newAccount);
+        Account output= testAccDAO.getAccount(accountNo);
+        try {
+            assertEquals(newAccount,output);
+        }
+        catch (AssertionError e) {
+
+        }
+    }
+
+    @Test
+    public void addAccount() throws InvalidAccountException {
+        String accountNo="809605B";
+        String bankName = "BMN";
+        String accountHolderName = "XYZ";
+        Double balance = 12000.0;
+        Account newAccount= new Account(accountNo,bankName,accountHolderName,balance);
+        PersistantMemoryAccountDAO testAccDAO= new PersistantMemoryAccountDAO(ApplicationProvider.getApplicationContext());
+        testAccDAO.addAccount(newAccount);
+        Account output= testAccDAO.getAccount(accountNo);
+        try {
+            assertEquals(newAccount,output);
+        }
+        catch (AssertionError e) {
+
+        }
+    }
+}
